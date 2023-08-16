d18c9b865e8f1dfa03e2b238287226661987fd31
Andrei Dogadaev <dogadlev@yandex.ru>
2023-08-16 10:27:58 +0300

    Initial commit.

---
+++ Source/LeaveMeAlone/Private/Player/LMAPlayerController.cpp
@@ -0,0 +1,13 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+#include "Player/LMAPlayerController.h"
+
+ALMAPlayerController::ALMAPlayerController() {}
+
+void ALMAPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetInputMode(FInputModeGameOnly());
+	bShowMouseCursor = false;
+}
---
+++ Source/LeaveMeAlone/Public/Player/LMADefaultCharacter.h
@@ -0,0 +1,64 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LMADefaultCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+class UInputComponent;
+class UDecalComponent;
+class UMaterialInterface;
+
+UCLASS()
+class LEAVEMEALONE_API ALMADefaultCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ALMADefaultCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	float MaxArmLength = 3000.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	float MinArmLength = 500.0f;
+
+	UPROPERTY()
+	UDecalComponent* CurrentCursor = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	UMaterialInterface* CursorMaterial = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	FVector CursorSize = FVector(20.0f, 40.0f, 40.0f);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	float YRotation = -75.0f;
+	float ArmLength = 1400.0f;
+	float FOV = 55.0f;
+
+	void MoveForward(float);
+	void MoveRight(float);
+	void ChangeCameraArmLength(float);
+};
